5da676bef802972e5728cf5f2f5e275492de6eb6
Krzysztof Doroszko <doroszko.k@gmail.com>
2019-10-17 19:58:22 +0200

    6th commit - add destructor

--- My_unique_ptr.hpp
+++ My_unique_ptr.hpp
@@ -5,6 +5,11 @@ public:
 
     My_unique_ptr(T *new_ptr) : ptr(new_ptr) {}
 
+    ~My_unique_ptr()
+    {
+        delete ptr;
+    }
+
 private:
 
     T *ptr;
--- main.cpp
+++ main.cpp
@@ -3,18 +3,14 @@
 
 #include "My_unique_ptr.hpp"
 
-void test_my_unique_ptr_stores_pointer_to_any_type_of_data()
+void no_test_add_destructor()
 {
-    int number = 1;
-    My_unique_ptr<int> ptr_to_int(&number);
-
-    double number2 = 2;
-    My_unique_ptr<double> ptr_to_double(&number2);
+    My_unique_ptr<int> (new int);
 }
 
 int main()
 {
-    test_my_unique_ptr_stores_pointer_to_any_type_of_data();
+    no_test_add_destructor();
 
     return 0;
 }
